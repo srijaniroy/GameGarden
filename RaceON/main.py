35b724c8fc2ce7b16cb88f4803d8b87a7c343232
srijaniroy <srijaniroy185@gmail.com>
2024-10-11 01:06:43 +0530

    Set up game window

---
+++ RaceON/main.py
@@ -0,0 +1,20 @@
+import pygame
+import time
+import math
+from utils import scale_image, blit_rotate_center
+
+GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+
+TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("RaceON!")
+
+FPS = 60
+
+pygame.quit()
